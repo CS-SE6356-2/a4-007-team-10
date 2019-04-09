f2cbaed4ee3af64bf50ffa56b103d2b33529d09d
John D'Angelo <qazzaq6@yahoo.com>
2019-04-08 23:54:00 -0500

    Add Balance Decrementing Test
    Test case to test removing a product updates the carts balance value

--- ProductTest.java
+++ ProductTest.java
@@ -18,20 +18,21 @@ class ProductTest {
 			assertTrue(newCount > oldCount);
 	}
 
-	//test 4 - balance update
+	//test 4 - balance adding update
 	@Test
 	public void BalanceUpdateTest() 
 	{
 			ShoppingCart cart = new ShoppingCart();
 			double balance = cart.getBalance();
 			cart.addItem(new Product("product1", 30.00));
+			
 			assertEquals(balance + 30.00, cart.getBalance());
 			//update balance
 			balance = cart.getBalance();
 			
 			//check again
 			cart.addItem(new Product("product2", 75.25));
-			assertEquals(balance + 75.25, example.getBalance());		
+			assertEquals(balance + 75.25, cart.getBalance());		
 	}
 
 	
@@ -51,6 +52,26 @@ class ProductTest {
 			int newCount = cart.getItemCount();
 
 			assertTrue(newCount < oldCount);
+	}
+	
+	// test 6 - balance removal update
+	public void BalanceDecreaseTest() 
+	{
+			ShoppingCart cart = new ShoppingCart();
+			double balance = cart.getBalance();
+			Product prod1 = new Product("product1", 30.00);
+			Product prod2 = new Product("product2", 15.00);
+			
+			cart.addItem(prod1);
+			cart.addItem(prod2);
+			
+			assertEquals(balance + 45.00, cart.getBalance());
+			//update balance
+			balance = cart.getBalance();
+			
+			//check again
+			cart.removeItem(prod2);
+			assertEquals(balance - 15.00, cart.getBalance());		
 	}
 
 }
