f14a44bd834405b650443e620d625b01c644c030
maiflys <mai_media@outlook.com>
2019-04-08 22:36:06 -0500

    Create TestRunner.java
    Creation of the Test runner

---
+++ TestRunner.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+class TestRunner {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
