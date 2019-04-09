35a9ad141139b7beb767c975edfa08bf0c93ded2
maiflys <mai_media@outlook.com>
2019-04-08 23:36:52 -0500

    multiple
    fixed spelling error in exception test,
    moved some Steve's work into product test, created balance test

---
+++ ProductTest.java
@@ -0,0 +1,56 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProductTest {
+
+	@Test
+	public void increaseSizeWhenAdded()										//test 3: increments size when new item is added
+	{
+			ShoppingCart cart = new ShoppingCart();
+			int oldCount = cart.getItemCount();
+
+			cart.addItem(new Product("Item1", 300.0));
+			cart.addItem(new Product("Item2", 500.0));
+
+			int newCount = cart.getItemCount();
+
+			assertTrue(newCount > oldCount);
+	}
+
+	//test 4 - balance update
+	@Test
+	public void BalanceUpdateTest() 
+	{
+			ShoppingCart cart = new ShoppingCart();
+			double balance = cart.getBalance();
+			cart.addItem(new Product("product1", 30.00));
+			assertEquals(balance + 30.00, cart.getBalance());
+			//update balance
+			balance = cart.getBalance();
+			
+			//check again
+			cart.addItem(new Product("product2", 75.25));
+			assertEquals(balance + 75.25, example.getBalance());		
+	}
+
+	
+	// test 5
+	@Test																									//test 5: decreases in size when item is removed
+	public void decreaseSizeWhenRemoved() throws ProductNotFoundException
+	{
+			ShoppingCart cart = new ShoppingCart();
+
+			cart.addItem(new Product("Item1", 300.0));
+			cart.addItem(new Product("Item2", 500.0));
+
+			int oldCount = cart.getItemCount();
+
+			cart.removeItem(new Product("Item1", 300.0));
+
+			int newCount = cart.getItemCount();
+
+			assertTrue(newCount < oldCount);
+	}
+
+}
