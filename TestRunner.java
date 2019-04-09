bc17526030b2f142cf80d99da53d17d2b4bdc999
Steve Johnson <spj160030@utdallas.edu>
2019-04-08 22:54:05 -0500

    Added test case 3 & 5
    I also added the JunitRunner class

--- TestRunner.java
+++ TestRunner.java
@@ -13,4 +13,36 @@ class TestRunner {
 		fail("Not yet implemented");
 	}
 
+	@Test
+	public void increaseSizeWhenAdded()										//test 3: increments size when new item is added
+	{
+			ShoppingCart cart = new ShoppingCart();
+			int oldCount = cart.getItemCount();
+
+			cart.addItem(new Product("Item1", 300.0));
+			cart.addItem(new Product("Item2", 500.0));
+
+			int newCount = cart.getItemCount();
+
+			assertTrue(newCount > oldCount);
+	}
+
+	@Test																									//test 5: decreases in size when item is removed
+	public void decreaseSizeWhenRemoved() throws ProductNotFoundException
+	{
+			ShoppingCart cart = new ShoppingCart();
+
+			cart.addItem(new Product("Item1", 300.0));
+			cart.addItem(new Product("Item2", 500.0));
+
+			int oldCount = cart.getItemCount();
+
+			cart.removeItem(new Product("Item1", 300.0));
+
+			int newCount = cart.getItemCount();
+
+			assertTrue(newCount < oldCount);
+	}
+}
+
 }
