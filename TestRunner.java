d8a613bebdf4853ace9746c7938d788a94922942
Steve Johnson <spj160030@utdallas.edu>
2019-04-08 23:02:38 -0500

    Removed JunitRunner and merged changes w/ TestRunner

--- TestRunner.java
+++ TestRunner.java
@@ -1,6 +1,4 @@
 import static org.junit.jupiter.api.Assertions.*;
-
-
 import org.junit.jupiter.api.Test;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
@@ -9,32 +7,32 @@ import org.junit.runner.notification.Failure;
 class TestRunner {
 
 	public static void main(String[] args) {
-		//Test ShoppingCart 
+		//Test ShoppingCart
 		Result SCresult = JUnitCore.runClasses(ShoppingCartTest.class);
 		for(Failure failure : result.getFailures()) {
 			System.out.println(failure.toString());
 		}
-	
+
 		System.out.println(result.wasSuccessful());
-		
+
 		//Test Product
 		Result productResult = JUnitCore.runClasses(ProductTest.class);
 		for(Failure failure : productResult.getFailures()) {
 			System.out.println(failure.toString());
 		}
-	
+
 		System.out.println(productResult.wasSuccessful());
-		
-		
+
+
 		//Test ProductNotFoundException
 		Result exceptionResult = JUnitCore.runClasses(ExceptionTest.class);
 		for(Failure failure : exceptionResult.getFailures()) {
 			System.out.println(failure.toString());
 		}
-	
+
 		System.out.println(exceptionResult.wasSuccessful());
 	}
-	
+
 
 	@Test
 	public void increaseSizeWhenAdded()										//test 3: increments size when new item is added
@@ -66,6 +64,4 @@ class TestRunner {
 
 			assertTrue(newCount < oldCount);
 	}
-}
-
 }
